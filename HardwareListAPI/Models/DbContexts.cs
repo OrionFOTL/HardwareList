7cc5a15db0d717fc2c2538004af517407b787a83
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-05-20 16:09:35 +0200

    added DbContexts

---
+++ HardwareListAPI/Models/DbContexts.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareListAPI.Models
+{
+    public class DbContexts
+    {
+        public DbContexts(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<CPU> CPUs { get; set; }
+        public DbSet<GPU> GPUs { get; set; }
+        public DbSet<HDD> HDDs { get; set; }
+        public DbSet<SSD> SSDs { get; set; }
+    }
+}
