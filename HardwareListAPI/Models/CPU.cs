b6079048790d435715a49b6b09910e51870ff401
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-05-20 15:59:46 +0200

    added hardware models

---
+++ HardwareListAPI/Models/CPU.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareListAPI.Models
+{
+    public class CPU
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Vendor { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int Frequency { get; set; }
+
+        [Required]
+        public int Voltage { get; set; }
+
+        [Required]
+        public int CinebenchSingleCore { get; set; }
+
+        [Required]
+        public int CinebenchMultiCore { get; set; }
+
+    }
+}
---
+++ HardwareListAPI/Models/GPU.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareListAPI.Models
+{
+    public class GPU
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Vendor { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public int CoreClock { get; set; }
+
+        [Required]
+        public int MemoryClock { get; set; }
+
+        [Required]
+        public int Voltage { get; set; }
+
+        [Required]
+        public int FireStrikeScore { get; set; }
+
+    }
+}
