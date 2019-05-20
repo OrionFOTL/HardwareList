b1c53c19249720493ae569c37d8b0470fdcb1a62
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-05-20 13:33:40 +0200

    Created project files

---
+++ HardwareListWebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HardwareListWebClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
