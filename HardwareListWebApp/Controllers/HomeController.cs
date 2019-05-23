25d20e56934af587f44543b4a20dbef2a75f4865
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-05-23 17:25:09 +0200

    added login and registration pages

--- HardwareListWebApp/Controllers/HomeController.cs
+++ HardwareListWebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HardwareListWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HardwareListWebApp.Controllers
 {
@@ -15,6 +16,7 @@ namespace HardwareListWebApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
