90caaaa2afc708a29e111529bf3c16322e5c7b00
OrionFOTL <8158804+OrionFOTL@users.noreply.github.com>
2019-05-24 16:48:35 +0200

    added beginning of part adding script

---
+++ HardwareListWebApp/wwwroot/js/addPartForm.js
@@ -0,0 +1,41 @@
+﻿class AddPartForm {
+
+    constructor() {
+        this._listeners = []
+        let addBtn = document.querySelector('#cpu_addBtn')
+        addBtn.addEventListener('click', e => {   //to zamiasat function(e) = {}
+            let vendorSelect = document.querySelector('#cpu_vendorSelect')
+            let modelInput = document.querySelector('#cpu_modelInput')
+            let frequencyInput = document.querySelector('#cpu_frequencyInput')
+            let voltageInput = document.querySelector('#cpu_voltageInput')
+            let cinesingleInput = document.querySelector('#cpu_cinesingleInput')
+            let cinemultiInput = document.querySelector('#cpu_cinemultiInput')
+            let obj = {
+                vendor: vendorSelect.value,
+                model: modelInput.value,
+                frequency: frequencyInput.value,
+                voltage: voltageInput.value,
+                cinesingle: cinesingleInput.value,
+                cinemulti: cinemultiInput.value
+            }
+
+            modelInput.value = ''
+            frequencyInput.value = ''
+            voltageInput.value = ''
+            cinesingleInput.value = ''
+            cinemultiInput.value = ''
+
+            this._raiseNewMeasurementAdded(obj)
+        })
+    }
+
+    addEventListener(listener) {
+        this._listeners.push(listener)
+    }
+
+    _raiseNewMeasurementAdded(e) {
+        this._listeners.forEach(l => {
+            l.newMeasurementAdded(e)
+        })
+    }
+}
